7dee163f33491d1d27684d4f5b79c57aa797650c
David Nilsson <nilsson.dd+github+gravatarobscurity@gmail.com>
2012-03-10 22:20:11 +0100

    Add skeleton of Boltzmann class.

---
+++ src/neural/Boltzmann.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <cstdint>
+
+#include "Boltzmann.h"
+
+Boltzmann::Boltzmann(grid_t assigned)
+{
+}
+
+grid_t Boltzmann::getGrid()
+{
+}
+
+void Boltzmann::simulate(bool step)
+{
+}

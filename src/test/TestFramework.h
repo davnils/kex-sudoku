0cf8219314b4e6db72fdb2489359ef93822f0036
David Nilsson <nilsson.dd+github+gravatarobscurity@gmail.com>
2012-03-24 16:20:56 +0100

    Update to use SudokuSolver and clean up header mess.

---
+++ src/test/TestFramework.h
@@ -0,0 +1,22 @@
+#ifndef TESTFRAMEWORK_H_
+#define TESTFRAMEWORK_H_
+
+typedef struct
+{
+  std::string algorithm;
+  float avg;
+  std::vector<float> timeStamps;
+} result_t;
+
+class TestFramework
+{
+  public:
+    TestFramework();
+    void addSolver(SudokuSolver * solver);
+    std::vector<result_t> runTests();
+
+  private:
+    std::vector<SudokuSolver> solvers;
+};
+
+#endif

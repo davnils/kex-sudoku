47fe9b8cdc0c50d8be0b6c052f7daa6a599e2089
Patrik Berggren <pabergg@gmail.com>
2012-04-10 12:03:11 +0200

    Changes to matlab files

--- analysis/matlabFiles/analysis.m
+++ analysis/matlabFiles/analysis.m
@@ -3,11 +3,32 @@ backtrack
 rulebased
 disp('Imported datafiles succesfully');
 disp('Histograms of the distributions of time are as follows');
-%hist(backtracktimes,500);
+bt = [];
+index = 1;
+unsolved = 0;
+unstable = 0;
+
+for i = 1:length(backtracktimes)
+    if(backtracktimes(i)>=0)
+        bt(index) = backtracktimes(i);
+        index = index + 1;
+    elseif(backtracktimes(i)==-1)
+        unstable = unstable + 1; 
+    else%(bakctracktimes(i)==-2)
+        unsolved = unsolved + 1;
+    end
+end
+
+fprintf('Unsolved %d\nUnstable %d\nSolved %d\nTotal %d\n',unsolved,unstable,length(bt),length(backtracktimes));
+        
+    
+
+hist(bt,500);
+axis([0 2 -Inf Inf]); 
 %pause;
 %hold on;
-hist(rulebasedtimes,500);
-set(gca,'XTick',0:0.05:2);
+%hist(rulebasedtimes,500);
+%set(gca,'XTick',0:0.1:2);
 % Create xlabel
 %xlabel({'Solution times (seconds)'});
 
